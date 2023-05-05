bd7594e95e2a759e6d47ee893e46c73aa3f10315
Reginaldo <porffiriocosta@gmail.com>
2023-05-05 17:36:10 -0300

    Criação formulario cadastro

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,6 +7,7 @@ import IceCreamsPage from './pages/Main/IceCreams'
 import PizzasPage from './pages/Main/Pizzas'
 
 import MyCartPage from './pages/MyCart'
+import Payment from './pages/Payment'
 
 export function AppRoutes() {
   return (
@@ -18,6 +19,7 @@ export function AppRoutes() {
         <Route path='ice-creams' element={<IceCreamsPage />} />
       </Route>
       <Route path='cart' element={<MyCartPage />} />
+      <Route path='payment' element={<Payment />} />
     </Routes>
   )
 }
